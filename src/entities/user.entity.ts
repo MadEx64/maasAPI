64356dd3fd538e47f46651c1de98af9829683c6a
Mickael Beites <mrouceirobeites@gmail.com>
2022-06-02 13:12:21 +0200

    feat:user domain and entity definition

---
+++ src/entities/user.entity.ts
@@ -0,0 +1,34 @@
+import { Exclude } from 'class-transformer';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  @Exclude()
+  password: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  constructor(data: Partial<User> = {}) {
+    Object.assign(this, data);
+  }
+}
