291eba48bb55c69d37ec6cd854efcfd8abc8c26a
Mickael Beites <mrouceirobeites@gmail.com>
2022-06-03 06:26:21 +0200

    feat(api): added swagger api properties

--- src/auth/dto/sign-up.dto.ts
+++ src/auth/dto/sign-up.dto.ts
@@ -1,16 +1,20 @@
 import { IsDefined, IsNotEmpty, IsEmail, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class SignUp {
   @IsDefined()
   @IsNotEmpty()
+  @ApiProperty()
   readonly name: string;
 
   @IsDefined()
   @IsEmail()
+  @ApiProperty()
   readonly email: string;
 
   @IsDefined()
   @IsNotEmpty()
   @MinLength(8)
+  @ApiProperty()
   readonly password: string;
 }
